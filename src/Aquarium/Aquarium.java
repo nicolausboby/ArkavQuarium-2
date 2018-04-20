68775eab4d239a7b98d80870a657585882914e91
RegiPurba <regipurba07@gmail.com>
2018-04-20 17:19:36 +0700

    Update Aquarium

--- src/Aquarium/Aquarium.java
+++ src/Aquarium/Aquarium.java
@@ -15,10 +15,26 @@ import Coin.*;
 public class Aquarium {
     private List<Fish> Fishes;
     private List<Coin> Coins;
-    private List<Food> Fodds;
+    private List<Food> Foods;
     Snail snail;
 
     public Aquarium() {
         this.Fishes = new ArrayList<>();
+    }
+    
+    public Snail getSnail(){
+        return this.snail;
+    }
+    
+    public List<Fish> getFishes(){
+        return this.Fishes;
+    }
+    
+    public List<Coin> getCoins(){
+        return this.Coins;
+    }
+    
+    public List<Food> getFood(){
+        return this.Foods;
     }
 }
