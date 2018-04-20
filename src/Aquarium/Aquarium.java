817078500b4b8063e36c43963652b60d665eece2
RegiPurba <regipurba07@gmail.com>
2018-04-20 17:12:35 +0700

    add Aquarium

---
+++ src/Aquarium/Aquarium.java
@@ -0,0 +1,24 @@
+/*
+ * ArkavQuarium
+ * 
+ */
+package Aquarium;
+
+import java.util.*;
+import Animal.*;
+import Coin.*;
+
+/**
+ *
+ * @author regipurba
+ */
+public class Aquarium {
+    private List<Fish> Fishes;
+    private List<Coin> Coins;
+    private List<Food> Fodds;
+    Snail snail;
+
+    public Aquarium() {
+        this.Fishes = new ArrayList<>();
+    }
+}
