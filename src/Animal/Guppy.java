609aaf47b1c018aff87ffb6d861190be600d88d7
RegiPurba <regipurba07@gmail.com>
2018-04-21 23:02:53 +0700

    update Guppy

--- src/Animal/Guppy.java
+++ src/Animal/Guppy.java
@@ -4,10 +4,25 @@
  */
 package Animal;
 
+import java.util.*;
 /**
  *
  * @author regipurba
  */
 public class Guppy {
+    private final int growTo2;
+    private final int grwoTo3;
+    private int growLevel;
+    private int nbFood;
+    private double coinTime;
+    public final double intervalGenerateCoin;
+    public Guppy(){
+        this.growTo2 = 3;
+        this.grwoTo3 = 10;
+        this.growLevel = 1;
+        this.coinTime = 0;
+        this.intervalGenerateCoin = 22000;
+    }
+    
     
 }
