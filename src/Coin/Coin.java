aabde893c874f010890234b0252a40f65f46ac03
RegiPurba <regipurba07@gmail.com>
2018-04-23 21:40:42 +0700

    update Coin

--- src/Coin/Coin.java
+++ src/Coin/Coin.java
@@ -16,7 +16,9 @@ public class Coin{
     private long value;
     private Location location;
     
-    
+    /**
+     *
+     */
     public Coin(){
         value = 0;
         statePic = 0;
@@ -24,6 +26,12 @@ public class Coin{
         location.setY(0);
     } 
     
+    /**
+     *
+     * @param value
+     * @param x
+     * @param y
+     */
     public Coin(int value, int x, int y){
         location.setX(x);
         location.setY(y);
@@ -31,28 +39,69 @@ public class Coin{
         statePic = 0;
     }
     
+    /**
+     *
+     * @param x
+     */
     public void setX(int x){location.setX(x);}
+
+    /**
+     *
+     * @param y
+     */
     public void setY(int y){location.setY(y);}
+
+    /**
+     *
+     * @return
+     */
     public int getX(){return location.getX();}
+
+    /**
+     *
+     * @return
+     */
     public int getY(){return location.getY();}
     
     //void Move(double deltatime); //Selalu move ke bawah
+
+    /**
+     *
+     * @return
+     */
     
     public long getValue(){
         return value;
     }
     
+    /**
+     *
+     * @param v
+     */
     public void setValue(long v) {
         value = v;
     }
     
+    /**
+     *
+     * @param s
+     */
     public void printCoin(String[] s){
         
     }
     
+    /**
+     *
+     * @return
+     */
     public int getStatePic(){
         return statePic;
     }
+
+    /**
+     *
+     * @param sp
+     */
     public void setStatePic(int sp){
         statePic = sp;
     }
