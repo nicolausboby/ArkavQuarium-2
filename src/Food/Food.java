c952afb3bc7b18d17d524ed3c71ff134e64a8ae3
RegiPurba <regipurba07@gmail.com>
2018-04-23 21:40:52 +0700

    update Food

--- src/Food/Food.java
+++ src/Food/Food.java
@@ -15,24 +15,52 @@ public class Food {
     private int statePic;
     private boolean isReachBottom;
     
+    /**
+     *
+     * @return
+     */
     public boolean getIsReachBottom() {
         return isReachBottom;
     }
     
+    /**
+     *
+     * @param isReachBottom
+     */
     public void setIsReachBottom(boolean isReachBottom){
         this.isReachBottom = isReachBottom;
     }
     
+    /**
+     *
+     * @param x
+     */
     public void setX(int x){
         location.setX(x);
     }
     
+    /**
+     *
+     * @param y
+     */
     public void setY(int y){
         location.setY(y);
     }
     
+    /**
+     *
+     * @return
+     */
     public int getX(){return location.getX();}
+
+    /**
+     *
+     * @return
+     */
     public int getY(){return location.getY();}
     
+    /**
+     *
+     */
     public void Move(){}
 }
