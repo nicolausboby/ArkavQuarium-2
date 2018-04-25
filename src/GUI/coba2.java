8a270ba4018c2832db3703a4c99f1911e2c9fb61
CisituKnight <36161373+aldoazali@users.noreply.github.com>
2018-04-25 23:18:28 +0700

    Add files via upload

--- src/GUI/coba2.java
+++ src/GUI/coba2.java
@@ -24,11 +24,11 @@ public class coba2 extends JFrame{
 	private Icon background = new ImageIcon("D:\\Informatics\\OOP\\Tubes-Java\\ArkavQuarium-2-master\\assets\\aquarium1.jpg");;
 	private Icon menubar = new ImageIcon("D:\\Informatics\\OOP\\Tubes-Java\\ArkavQuarium-2-master\\assets\\MenuBar.gif");;
 	private Icon iconGuppy = new ImageIcon("D:\\Informatics\\OOP\\Tubes-Java\\ArkavQuarium-2-master\\assets\\Guppy1Swim.gif");
-	private Icon iconPiranha;
-	private Icon iconSnail;
-	private Icon Egg1; 
-	private Icon Egg2;
-	private Icon Egg3;
+	private Icon iconPiranha = new ImageIcon(new ImageIcon("D:\\Informatics\\OOP\\Tubes-Java\\ArkavQuarium-2-master\\assets\\PiranhaSwim.gif").getImage().getScaledInstance(40,40,Image.SCALE_DEFAULT));
+	private Icon iconSnail = new ImageIcon("D:\\Informatics\\OOP\\Tubes-Java\\ArkavQuarium-2-master\\assets\\SnailStop.gif");
+	private Icon Egg1 = new ImageIcon("D:\\Informatics\\OOP\\Tubes-Java\\ArkavQuarium-2-master\\assets\\egg_1.png"); 
+	private Icon Egg2 = new ImageIcon("D:\\Informatics\\OOP\\Tubes-Java\\ArkavQuarium-2-master\\assets\\egg_2.png"); 
+	private Icon Egg3 = new ImageIcon("D:\\Informatics\\OOP\\Tubes-Java\\ArkavQuarium-2-master\\assets\\egg_3.png"); 
 	
 	private JLabel bg;
 	private JLabel bar;
@@ -36,24 +36,34 @@ public class coba2 extends JFrame{
 	
 	public coba2() {
 		buyGuppy = new JButton(iconGuppy);
-		buyGuppy.setBounds(25,5,40,40);
+		buyGuppy.setBounds(18,0,56,56);
 		buyGuppy.setBorderPainted(false);
 		buyGuppy.setBorder(null);
-		buyGuppy.setMargin(new Insets(0,0,0,0));
+		//buyGuppy.setMargin(new Insets(0,0,0,0));
 		buyGuppy.setContentAreaFilled(false);
 		buyGuppy.setIcon(iconGuppy);
 		buyGuppy.setPressedIcon(iconGuppy);
 		buyGuppy.setDisabledIcon(iconGuppy);
 		
 		buyPiranha = new JButton(iconPiranha);
-		buyPiranha.setBounds(65,45,40,40);
+		buyPiranha.setBounds(90,0,56,56);
 		buyPiranha.setBorderPainted(false);
 		buyPiranha.setBorder(null);
-		buyPiranha.setMargin(new Insets(0,0,0,0));
+		//buyPiranha.setMargin(new Insets(0,0,0,0));
 		buyPiranha.setContentAreaFilled(false);
-		buyPiranha.setIcon(iconGuppy);
-		buyPiranha.setPressedIcon(iconGuppy);
-		buyPiranha.setDisabledIcon(iconGuppy);
+		buyPiranha.setIcon(iconPiranha);
+		buyPiranha.setPressedIcon(iconPiranha);
+		buyPiranha.setDisabledIcon(iconPiranha);
+		
+		buyEgg = new JButton(Egg1);
+		buyEgg.setBounds(440,0,56,56);
+		buyEgg.setBorderPainted(false);
+		buyEgg.setBorder(null);
+		//buyEgg.setMargin(new Insets(0,0,0,0));
+		buyEgg.setContentAreaFilled(false);
+		buyEgg.setIcon(Egg1);
+		buyEgg.setPressedIcon(Egg1);
+		buyEgg.setDisabledIcon(Egg1);
 		
 		bg = new JLabel(background);
 		bg.setBounds(0, 0, 640, 480);
@@ -61,7 +71,8 @@ public class coba2 extends JFrame{
 		bar.setBounds(0, 0, 640, 72);
 		
 		frame.add(buyGuppy);
-
+		frame.add(buyPiranha);
+		frame.add(buyEgg);
 		frame.add(bar);
 		frame.add(bg);
 		
